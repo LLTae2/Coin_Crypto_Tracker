c37e461c7735beb0a3bbbf8b77abd1a0be39c72a
LLTae2 <taehyun5820@gmail.com>
2022-11-24 12:06:24 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,9 @@
 import React, { useState } from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
+import Router from "./Router";
 
 function App() {
-  const [username, setUsername] = useState("");
-
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    console.log(event.currentTarget.value);
-    const {
-      currentTarget: { value },
-    } = event;
-    setUsername(value);
-    // setUsername(event.currentTarget.value) 와 같은 의미
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", username);
-  };
-
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={username}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
-  );
+  return <Router />;
 }
 
 export default App;
