9d7f521f4d15cf3e8d04a47fe3e18208412459a0
LLTae2 <taehyun5820@gmail.com>
2022-11-24 11:13:06 +0900

    typescript::typescript와 theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import styled from "styled-components";
-import Circle from "./Circle";
+import React, { useState } from "react";
+// import styled from "styled-components";
 
 function App() {
+  const [username, setUsername] = useState("");
+
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    console.log(event.currentTarget.value);
+    const {
+      currentTarget: { value },
+    } = event;
+    setUsername(value);
+    // setUsername(event.currentTarget.value) 와 같은 의미
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", username);
+  };
+
   return (
     <div>
-      <Circle borderColor="black" bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={username}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
